dc2a7569200347b0c3437865bd91330ca91770d4
proapplab <proapplabs@hotmail.com>
2023-05-10 03:31:22 +0300

    Update about.js

--- components/home/about.js
+++ components/home/about.js
@@ -11,10 +11,10 @@ export default function About() {
         <style jsx>{`
         .About { background-color: var(--bg-card-color); padding: 30px; text-align: center; border-radius: var(--radius); margin-bottom: 30px; }
 .About * { margin: 0; }
-.About h5 { color: white; font-size: 13px; font-weight: normal; margin-bottom: 0px; }
-.About h6 { color: var(--text-color); font-size: 20px; font-weight: normal; margin-bottom: 20px; }
-.About p { font-size: 13px; width:70%;margin:auto; }
-.About img { width: 50%; }
+.About h5 { color: var(--text-color); font-size: 12px; font-weight: bold; margin-bottom: 7px; }
+.About h6 { color: var(--white); font-size: 18px; font-weight: 700; margin-bottom: 20px; }
+.About p { font-size: 9px; width:70%;margin:auto; }
+.About img { width: 60%; }
 {/* .About .icon { width: 100%;background-image:url('/v1/icon_about.png');height:200px;background-size:contain;background-repeat:no-repeat;margin:auto;background-position:center;} */}
 .About a { display: inline-block; background: linear-gradient(90deg, #ffdb92, #b18834); color: white; padding: 10px 30px; border-radius: 100px; font-weight: 600; letter-spacing: 1px; font-size: 16px; filter: drop-shadow(0 15px 30px rgba(0, 0, 0, 0.5)); }
 @media (max-width:960px) {
